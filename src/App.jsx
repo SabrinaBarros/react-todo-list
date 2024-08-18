5f6c80646d734626fc6c0e19b68186ebd371acf8
Sabrina Barros <sabrina.barros2045@gmail.com>
2024-08-17 23:58:36 -0300

    feat: listing initial tasks via api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const resAPI = () => {
+
+      fetch("https://my-json-server.typicode.com/EnkiGroup/DesafioReactFrontendJunior2024/todos")
+      .then(res => {
+
+        return res.json();
+
+      })
+      .then(data => {
+
+        setList(data);
+
+      });
+
+    };
+
+    resAPI();
+
+  }, []);
+
+    const task = list.map((item, i) => {
+      return (
+
+        <li className='list__item' key={item.id}> {item.title} </li>
+
+      );
+    });
+
+    console.log(task);
+
   return (
     <>
+      <input type="text" placeholder="What needs to be done?"></input>
+      <button>Send</button>
 
+      <ul className='list'>
+        {task}
+      </ul>
     </>
   )
 }
